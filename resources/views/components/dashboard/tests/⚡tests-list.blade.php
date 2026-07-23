27f74f2855d7a40a96e1e01528bb2947c66a66a1
mohamadtorki <mohamadprograming123@gmail.com>
2026-07-23 18:20:35 +0330

    add search exam to panel

--- resources/views/components/dashboard/tests/⚡tests-list.blade.php
+++ resources/views/components/dashboard/tests/⚡tests-list.blade.php
@@ -10,11 +10,17 @@ use Livewire\WithPagination;
 
 new #[Layout('layouts::dashboard')] #[Title('لیست آزمون ها')] class extends Component {
     use WithPagination, WithoutUrlPagination;
+    public $search;
 
     #[Computed]
     public function exams()
     {
-        return Exam::latest()->paginate(7);
+        return Exam::query()->when($this->search,function ($query){
+            $query->where('title','like',"%{$this->search}%")
+                ->orwhere('description','like',"%{$this->search}%")
+                ->orwhere('status','like',"%{$this->search}%");
+        })->latest()
+            ->paginate(7);
     }
 
     public function deleteExam($id)
@@ -34,6 +40,29 @@ new #[Layout('layouts::dashboard')] #[Title('لیست آزمون ها')] class e
         <h2 class="text-xl font-bold">
             لیست آزمون‌ها
         </h2>
+
+        <div class="relative w-[350px]">
+
+            <input wire:model.live.debounce.100ms="search"
+                type="text"
+                placeholder="جستجوی آزمون..."
+                class="w-full border border-gray-300 rounded-lg pr-10 pl-4 py-2 outline-none
+               focus:ring-2 focus:ring-blue-500 focus:border-blue-500 transition">
+
+            <svg xmlns="http://www.w3.org/2000/svg"
+                 class="absolute right-3 top-1/2 -translate-y-1/2 w-5 h-5 text-gray-400"
+                 fill="none"
+                 viewBox="0 0 24 24"
+                 stroke="currentColor">
+
+                <path stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      d="M21 21l-4.35-4.35m1.85-5.15a7 7 0 11-14 0 7 7 0 0114 0z"/>
+
+            </svg>
+
+        </div>
 
         <a @click="test=true"
            class="bg-blue-600 px-4 py-2 rounded-lg cursor-pointer hover:bg-blue-700 transition">
@@ -134,7 +163,7 @@ new #[Layout('layouts::dashboard')] #[Title('لیست آزمون ها')] class e
                 </tr>
             @empty
                 <tr>
-                    <td class="text-center p-4"></td>
+                    <td class="text-center p-4 text-red-500">آزمونی وجود ندارد</td>
                 </tr>
             @endforelse
 
